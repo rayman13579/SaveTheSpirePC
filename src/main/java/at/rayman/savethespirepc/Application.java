e5f163be6d2c33ca91b0623f797178475fe29662
rayman13579 <roman.reno@hotmail.com>
2024-10-25 19:06:15 +0200

    init

---
+++ src/main/java/at/rayman/savethespirepc/Application.java
@@ -0,0 +1,25 @@
+package at.rayman.savethespirepc;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 250, 220);
+        stage.setTitle("SaveTheSpire");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
